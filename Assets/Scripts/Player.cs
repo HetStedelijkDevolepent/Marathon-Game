797374a78f5015a42c13612fac5fa2066d71cca1
JustGamingLTD <support@justgamingltd.nl>
2018-12-20 09:47:06 +0100

    Test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,25 @@
 
 public class Player : MonoBehaviour {
 
+    Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     public void Update()
     {
         float xMov = Input.GetAxis("Horizontal");
         float yMov = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(xMov, yMov) * Time.deltaTime;
+        if(yMov > 0.2)
+        {
+            rb.AddForce(new Vector2(0, 100f));
+        }
+
+        transform.position += new Vector3(xMov, 0) * Time.deltaTime;
 
     }
 
