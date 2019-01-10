5bf01c1b16c318ac27b09e74d2f35607629b505c
JustGamingLTD <support@justgamingltd.nl>
2019-01-10 13:12:08 +0100

    Added player health and enemy health

---
+++ Assets/FlyingShootingEnemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FlyingShootingEnemy : Enemy
+{
+    [SerializeField]
+    int minrange;
+
+    [SerializeField]
+    int maxrange;
+
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, Player.instance.transform.position) < maxrange && Vector3.Distance(transform.position, Player.instance.transform.position) > minrange)
+            transform.position += (Player.instance.transform.position - transform.position).normalized * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FlyingEnemy : Enemy {
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, Player.instance.transform.position) < 15f)
+            transform.position += (Player.instance.transform.position - transform.position).normalized * Time.deltaTime;
+    }
+
+    
+
+        
+    
+
+}
